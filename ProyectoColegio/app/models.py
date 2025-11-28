fe193d65e11926cbc7dc31e4e00b08060222d459
JuanGuillermoPerezVega <jperezv2@soy.sena.edu.co>
2025-11-27 22:23:21 -0500

    Se agregaron los models elemento y unidadmedida

--- ProyectoColegio/app/models.py
+++ ProyectoColegio/app/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class UnidadMedida(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Unidad de Medida"
+        verbose_name_plural = "Unidades de Medida"
+        db_table = "unidadmedida"
+
+    def __str__(self):
+        return self.nombre
+class Elemento(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    stockActual = models.IntegerField()
+    stockMinimo = models.IntegerField()
+    tipoElementoId = models.ForeignKey(tipoelemento, on_delete=models.CASCADE)
+    categoriaId = models.ForeignKey(categoria, on_delete=models.CASCADE)
+    marcaId = models.ForeignKey(marca, on_delete=models.CASCADE)
+    unidadMedidaId = models.ForeignKey(unidadmedida, on_delete=models.CASCADE)
+    ubicacion = models.CharField(max_length=100)
+    fechaCreacion = models.DateTimeField(auto_now_add=True)
+    fechaActualizacion = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Elemento"
+        verbose_name_plural = "Elementos"
+        db_table = "elemento"
+
+    def __str__(self):
+        return self.nombre
+
