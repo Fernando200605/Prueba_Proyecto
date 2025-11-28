1162b0f9a27abbd451fd7a41dc4ef50cbb07e9dd
David Fernando Monroy <ferchitomonroy@gmail.com>
2025-11-27 21:51:42 -0500

    Se agregaron los models Usuario y Administrador

--- ProyectoColegio/app/models.py
+++ ProyectoColegio/app/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    contraseña = models.CharField(max_length=100)
+    estado = models.BooleanField(default=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_actualizacion = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+        db_table = "usuario"
+
+    def __str__(self):
+        return self.nombre
+
+
+class Administrador(models.Model):
+    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE, primary_key=True)
+    cargo = models.CharField(max_length=100)
+    class Meta:
+        verbose_name = "Administrador"
+        verbose_name_plural = "Administradores"
+        db_table = "administrador"
+        
+    def __str__(self):
+        return self.usuario.nombre
+    
+    
+
+
