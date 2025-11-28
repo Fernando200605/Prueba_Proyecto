985e890c4ff1b2a1be7153d6cb5a1aeee38790c2
JuanGuillermoPerezVega <jperezv2@soy.sena.edu.co>
2025-11-28 07:32:47 -0500

    Se agregaron los models categoria,tipoelemento y marca

--- ProyectoColegio/app/models.py
+++ ProyectoColegio/app/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+
+class categoria(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        db_table = "categoria"
+
+    def __str__(self):
+        return self.nombre
+    
+class tipoelemento(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "tipoelemento"
+        verbose_name_plural = "tipoelementos"
+        db_table = "tipoelemento"
+
+    def __str__(self):
+        return self.nombre
+    
+class marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "marca"
+        verbose_name_plural = "marcas"
+        db_table = "marca"
+
+    def __str__(self):
+        return self.nombre
+
+
